48584f3fc69d0f40154b7a11e3bc35a0e08b7d74
James <jamezbrown100@gmail.com>
2017-12-03 12:16:26 -0600

    Add link and isActive helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def isActive(controller, action = nil)
+    if action == nil
+      if params[:controller] == controller
+        'active'
+      elsif controller.include? params[:controller]
+        'active'
+      end
+    elsif params[:controller] == controller && params[:action] == action
+      'active'
+    elsif params[:controller] == controller && (action.include? params[:action])
+      'active'
+    end
+  end
+
 end
