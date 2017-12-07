08e813b10626c6e332b61c0c32f360bed8269ac0
James <jamezbrown100@gmail.com>
2017-12-07 16:46:38 -0600

    exercise index

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,8 +1,29 @@
 class ExercisesController < ApplicationController
   def index
+    @exercises = Exercise.all
   end
 
   def new
     @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+
+    if @exercise.save
+      flash[:success] = "Workout successfully saved"
+      redirect_to exercises_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @exercise = Exercise.find(params[:id])
+  end
+
+  private
+  def exercise_params
+    params.require(:exercise).permit(:name)
   end
 end
