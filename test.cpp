daca66bef2da035a019054df48a474e9d549a711
James <jamezbrown100@gmail.com>
2017-12-07 18:20:53 -0600

    Show page

---
+++ test.cpp
@@ -0,0 +1,10 @@
+
+
+string jobTitles[ITEMS]; //Array to store jobs
+double jobWages[ITEMS]; //Array to store wages
+
+// Set jobTitles and job Wages to zero
+for (string&foo : jobTitles)
+  foo = " ";
+for (double&baz : jobWages)
+  baz = 0.0;
